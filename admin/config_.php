b25fce1a359cd6b2b37967f46319757b175bd2cc
PSJ <paisijian@firstcom.com.sg>
2018-03-21 09:51:24 +0800

    Initial commit

---
+++ admin/config_.php
@@ -0,0 +1,37 @@
+<?php
+// Hackes
+define('URL', $_SERVER['HTTP_HOST'] . str_replace('/admin', '', dirname($_SERVER['PHP_SELF']))."/");
+define('BASE_DIR', str_replace(DIRECTORY_SEPARATOR,"/",str_replace(DIRECTORY_SEPARATOR . "admin", "", realpath(dirname(__FILE__))))."/");
+
+// HTTP Protocol
+define('HTTP_SERVER', 'http://'. URL. 'admin/');
+define('HTTPS_SERVER', 'http://'. URL. 'admin/');
+
+// HTTP Protocol Catalog
+define('HTTP_CATALOG', 'http://'. URL);
+define('HTTPS_CATALOG', 'http://'. URL);
+
+// Directory
+define('DIR_APPLICATION', BASE_DIR. 'admin/');
+define('DIR_SYSTEM', BASE_DIR. 'system/');
+define('DIR_IMAGE', BASE_DIR. 'image/');
+define('DIR_LANGUAGE', BASE_DIR. 'admin/language/');
+define('DIR_TEMPLATE', BASE_DIR. 'admin/view/template/');
+define('DIR_CONFIG', BASE_DIR. 'system/config/');
+define('DIR_CACHE', BASE_DIR. 'system/storage/cache/');
+define('DIR_DOWNLOAD', BASE_DIR. 'system/storage/download/');
+define('DIR_LOGS', BASE_DIR. 'system/storage/logs/');
+define('DIR_MODIFICATION', BASE_DIR. 'system/storage/modification/');
+define('DIR_UPLOAD', BASE_DIR. 'system/storage/upload/');
+define('DIR_CATALOG', BASE_DIR. 'catalog/');
+define('DIR_EXCEL', BASE_DIR. 'system/storage/excel/');
+// define('DIR_EXCEL', BASE_DIR. 'system/storage/excel/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'estee');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'puo1jdc7_shv14v62_');
